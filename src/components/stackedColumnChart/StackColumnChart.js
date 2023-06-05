1b0af05640943e56eded10206ce08789b6bb2ec5
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-06-06 04:57:09 +0700

    update

--- src/components/stackedColumnChart/StackColumnChart.js
+++ src/components/stackedColumnChart/StackColumnChart.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
  
-var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class StackedColumnChart extends Component {
