4cf1f997422fc5d605423afa028cd5668b01c178
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-06-18 21:48:18 +0700

    add

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,5 @@
 import styles from './Project.module.css';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import MainLayout from '../../components/layout/MainLayout';
 import {useParams} from 'react-router-dom';
 import projectsSampleData from '../../data/projects';
@@ -334,14 +334,60 @@ const MembersTab = () => {
 }
 
 const AssignmentsTab = () => {
+	const [activeKey, setActiveKey] = useState(initialItems[0].key);
+	const [items, setItems] = useState(initialItems);
+	const newTabIndex = useRef(0);
+
+	const onChange = (newActiveKey) => {
+		setActiveKey(newActiveKey);
+	};
+	const add = () => {
+		const newActiveKey = `newTab${newTabIndex.current++}`;
+		const newPanes = [...items];
+		newPanes.push({
+		label: 'New Tab',
+		children: 'Content of new Tab',
+		key: newActiveKey,
+		});
+		setItems(newPanes);
+		setActiveKey(newActiveKey);
+  	};
+
+	const remove = (targetKey) => {
+		let newActiveKey = activeKey;
+		let lastIndex = -1;
+		items.forEach((item, i) => {
+		if (item.key === targetKey) {
+			lastIndex = i - 1;
+		}
+		});
+		const newPanes = items.filter((item) => item.key !== targetKey);
+		if (newPanes.length && newActiveKey === targetKey) {
+		if (lastIndex >= 0) {
+			newActiveKey = newPanes[lastIndex].key;
+		} else {
+			newActiveKey = newPanes[0].key;
+		}
+		}
+		setItems(newPanes);
+		setActiveKey(newActiveKey);
+	};
+
+	const onEdit = (targetKey, action) => {
+		if (action === 'add') {
+		add();
+		} else {
+		remove(targetKey);
+		}
+	};
 
 	return (
 		<div>
 			<Tabs
 				type="editable-card"
-				// onChange={onChange}
-				// activeKey={activeKey}
-				// onEdit={onEdit}
+				onChange={onChange}
+				activeKey={activeKey}
+				onEdit={onEdit}
 				items={initialItems}
     		/>
 		</div>
